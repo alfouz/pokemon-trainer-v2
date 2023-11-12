539ce1ebc1433a1b76d211d331cdbd0816500768
alfouz <adrian.lfouz@gmail.com>
2023-11-12 12:22:54 +0100

    add zone animation

--- src/components/PokemonZone/PokemonZone.styles.tsx
+++ src/components/PokemonZone/PokemonZone.styles.tsx
@@ -1,14 +1,17 @@
-import styled from "styled-components";
+import styled, { css, keyframes } from "styled-components";
 
 export const Container = styled.div`
   width: 50px;
   height: 50px;
   margin: 4px;
-  border: 1px solid black;
-  &:active {
-    border: 1px solid rgb(182, 182, 182);
-  }
 `;
+
+const VibrateAnimation = keyframes`
+ 0% { transform: scale(1.05) }
+ 50% { transform: scale(1.00) }
+ 100% { transform: scale(1.05) }
+`;
+
 export const StyledImage = styled.img<{ selected: boolean }>`
   image-rendering: pixelated;
   filter: ${({ selected }) => (selected ? "grayscale(0)" : "grayscale(1)")};
@@ -17,4 +20,14 @@ export const StyledImage = styled.img<{ selected: boolean }>`
     filter: grayscale(0);
   }
   cursor: pointer;
+  animation: ${({ selected }) =>
+    selected
+      ? css`
+          ${VibrateAnimation} 1s linear infinite
+        `
+      : "none"};
+  border: 1px solid black;
+  &:active {
+    border: 1px solid rgb(182, 182, 182);
+  }
 `;

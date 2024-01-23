667fcbc2a275b1357f5e29a5d91d7c63ca1df48b
alfouz <adrian.lfouz@gmail.com>
2024-01-23 17:15:05 +0100

    update game version

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,7 +7,7 @@ const githubLink = "https://github.com/alfouz/pokemon-trainer-v2";
 const Footer = () => {
   return (
     <Container>
-      Game made by alfouz V.0.1
+      Game made by alfouz V.0.2
       <StyledAnchor href={githubLink} target="_blank">
         <StyledImage src={GithubIcon} width={20} height={20} />
       </StyledAnchor>
